9a4a3eed40e3a0029cf5fce48f10684190b9842e
Nils Urbach <ndu01u@gmail.com>
2023-10-06 00:03:21 +0200

    now also git knows about the tests

--- _core/json_settings.py
+++ _core/json_settings.py
@@ -1,5 +1,6 @@
 import json_convenience as jsonx
 from pathlib import Path
+from typing import Any
 
 # TODO: add documentation
 # TODO: make it its own thing and add to git / pip
@@ -8,23 +9,26 @@ from pathlib import Path
 Setting = jsonx.Property
 
 
-class Settings:
+class Settings(object):
     def __init__(self, path: Path):
         if not path.exists():
             raise FileNotFoundError(f"{path} doesn't exist")
         elif not path.is_file() or not path.suffix == ".json":
             raise FileNotFoundError(f"{path} is no .json file")
         self.__dict__["_all_keys"] = tuple(jsonx.read_json_file(file_path=path).keys())
-        self._file = path
+        self.__dict__["_file"] = path
 
     def __getattr__(self, name: str) -> Setting:
         if name not in self.__dict__:
             self.__dict__[name] = jsonx.get_property(file_path=self._file, keys=(name, ))
         return self.__dict__[name]
 
-    def __setattr__(self, name: str, value: Setting) -> None:
+    def __setattr__(self, name: str, value: Any) -> None:
         if name not in self._all_keys:
-            self.__dict__[name] = value
+            if name not in self.__dict__.keys():
+                raise AttributeError(f"'{self.__class__}' object has no setting or attribute '{name}'")
+            else:
+                self.__dict__[name] = value
             return
         if not isinstance(value, Setting):
             raise jsonx.NotAPropertyError(no_property_object=value)
